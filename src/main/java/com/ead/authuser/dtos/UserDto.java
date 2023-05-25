e718161ef022cc14ceb7391d462fcbbf7d13bd09
Francisco Lima <francisco.lima@liferay.com>
2023-05-25 20:00:51 -0300

    add user controller endpoint for updates

--- src/main/java/com/ead/authuser/dtos/UserDto.java
+++ src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
 import java.util.UUID;
@@ -9,14 +10,36 @@ import java.util.UUID;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
 
+    public interface UserView {
+        public static interface RegistratioPost {}
+        public static interface UserPut {}
+        public static interface PasswordPut {}
+        public static interface ImagePut {}
+    }
+
     private UUID userId;
+    @JsonView(UserView.RegistratioPost.class)
     private String username;
+
+    @JsonView(UserView.RegistratioPost.class)
     private String email;
+
+    @JsonView({UserView.RegistratioPost.class, UserView.PasswordPut.class})
     private String password;
+
+    @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
+
+    @JsonView({UserView.RegistratioPost.class, UserView.UserPut.class})
     private String fullName;
+
+    @JsonView({UserView.RegistratioPost.class, UserView.UserPut.class})
     private String phoneNumber;
+
+    @JsonView({UserView.RegistratioPost.class, UserView.UserPut.class})
     private String cpf;
+
+    @JsonView(UserView.ImagePut.class)
     private String imageUrl;
 
 }
