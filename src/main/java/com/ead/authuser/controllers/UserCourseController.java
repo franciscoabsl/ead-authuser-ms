d3443728b172e41dff841f5cb0976ca82e9d9f64
Francisco Lima <francisco.lima@liferay.com>
2023-06-11 14:47:17 -0300

    add get all courses by user id - second part

--- src/main/java/com/ead/authuser/controllers/UserCourseController.java
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -1,7 +1,9 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.clients.*;
 import com.ead.authuser.dtos.*;
 import lombok.extern.log4j.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.data.domain.*;
 import org.springframework.data.web.*;
 import org.springframework.http.*;
@@ -14,13 +16,14 @@ import java.util.*;
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserCourseController {
 
+    @Autowired
+    UserClient userClient;
 
     @GetMapping("/users/{userId}/courses")
     public ResponseEntity<Page<CourseDto>> getAllCoursesByUser(
             @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
             @PathVariable(value = "userId") UUID userId
     ) {
-
-        return ResponseEntity.status(HttpStatus.OK).body(null);
+        return ResponseEntity.status(HttpStatus.OK).body(userClient.getAllCoursesByUser(userId, pageable));
     }
 }
