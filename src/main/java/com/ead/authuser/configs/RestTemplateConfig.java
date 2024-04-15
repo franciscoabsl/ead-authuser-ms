04ae317803b039dc5750502f619ffa6621dfa6b8
Francisco Lima <francisco.lima@liferay.com>
2024-04-15 14:39:04 -0300

    enable eureka client

--- src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -1,12 +1,14 @@
 package com.ead.authuser.configs;
 
 import org.springframework.boot.web.client.*;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.*;
 import org.springframework.web.client.*;
 
 @Configuration
 public class RestTemplateConfig {
 
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         // do any additional configuration here
